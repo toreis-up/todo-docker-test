5c470fe5e5f0829cb5e2258f393c020a0eab7f16
tores-up <toreis@reisan.dev>
2023-12-31 10:57:14 +0900

    b feat: find todo by id

--- backend/app/controllers/todos_controller.rb
+++ backend/app/controllers/todos_controller.rb
@@ -1,7 +1,19 @@
 class TodosController < ApplicationController
+  before_action :set_todo, only: %i[ show ]
+
   # GET /todos
   def index
     @todos = Todo.all
-    render status: 200, json: {todos: @todos}
+    render json: { items: @todos }
   end
+
+  # GET /todos/:id
+  def show
+    render json: { item: @todo}
+  end
+
+  private
+    def set_todo
+      @todo = Todo.find(params[:id])
+    end
 end
