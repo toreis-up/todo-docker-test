d00438e57b605de2aa519cef02e80628ddc1638a
tores-up <toreis@reisan.dev>
2023-12-31 11:01:03 +0900

    b feat: create todo

--- backend/app/controllers/todos_controller.rb
+++ backend/app/controllers/todos_controller.rb
@@ -12,8 +12,23 @@ class TodosController < ApplicationController
     render json: { item: @todo}
   end
 
+  # POST /todos
+  def create
+    @todo = Todo.new(todo_params)
+
+    if @todo.save
+      render json: @todo, status: :created, location: @todo
+    else
+      render json: @todo.errors, status: :unprocessable_entity
+    end
+  end
+
   private
     def set_todo
       @todo = Todo.find(params[:id])
+    end
+
+    def todo_params
+      params.require(:todo).permit(:title, :detail)
     end
 end
