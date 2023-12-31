16b621520ad6d3d0af5de5bff579172000e4b65a
tores-up <toreis@reisan.dev>
2023-12-31 11:34:01 +0900

    b feat: set timezone

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -27,6 +27,7 @@ module Ruby
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
+    config.time_zone = 'Asia/Tokyo'
 
     # Configuration for the application, engines, and railties goes here.
     #
