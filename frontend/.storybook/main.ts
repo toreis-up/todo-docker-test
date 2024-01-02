3bfa3c19f783b902ea6bafa1760f86b2f5f1906a
tores-up <toreis@reisan.dev>
2024-01-02 20:57:33 +0900

    f chore: setup storybook

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -2,7 +2,8 @@ import { StorybookConfig } from "@storybook/vue3-vite";
 
 const config: StorybookConfig = {
   framework: "@storybook/vue3-vite",
-  stories: ["../src/**/*.stories.@(js|ts)"]
+  stories: ["../src/**/*.stories.@(js|ts)"],
+  addons: ["@storybook/addon-controls", "@storybook/addon-actions", "@storybook/addon-viewport",],
 };
 
 export default config;
