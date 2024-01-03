2707f1073721ed22cda0dde5604fa63ea57da913
tores-up <toreis@reisan.dev>
2024-01-03 23:30:08 +0900

    f feat: Todo Card List

---
+++ frontend/src/components/CardList.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+import { PropType, defineComponent } from 'vue';
+import Card from './Todo/Card.vue';
+import { TodosGet200Response } from '~/types';
+export default defineComponent({
+  components: {Card},
+  props: {
+    items: {
+      type: Object as PropType<TodosGet200Response>,
+      required: true,
+    }
+  },
+  methods: {
+    onToggleCheckbox(e: {status: Boolean, id: Number}) {
+      console.log(e)
+      this.$emit('change', e);
+    }
+  },
+  emits: ['change']
+})
+</script>
+
+<template>
+  <div v-for="(item) in items.items">
+    <Card class="card" :title="item.title || 'Not Set'" :is-done="item.isDone" @change="(e) => onToggleCheckbox({...e, id: item.id})" />
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
