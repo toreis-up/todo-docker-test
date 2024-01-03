6fb273499d780098d4a2f53ee1c8ec7c17d4009d
tores-up <toreis@reisan.dev>
2024-01-03 23:28:37 +0900

    f feat: CheckBox for Todo Card

---
+++ frontend/src/components/Todo/CardParts/CheckBox.vue
@@ -0,0 +1,34 @@
+<template>
+    <input type="checkbox" v-model="checked" :disabled="disabled" :checked="done" @change="onToggleChange()" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  data() {
+    return {
+      checked: this.done
+    }
+  },
+  props: {
+    done: {
+      type: Boolean,
+      default: () => false
+    },
+    disabled: {
+      type: Boolean,
+      default: () => false
+    }
+  },
+  methods: {
+    onToggleChange() {
+      console.log({status: this.checked});
+      this.$emit('change', {status: this.checked})
+    }
+  },
+  emits: ['change']
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
