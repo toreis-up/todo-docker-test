1675c02070f56b878a25a648d98241b9b56c6fb7
tores-up <toreis@reisan.dev>
2024-01-03 23:27:56 +0900

    f feat: Header

---
+++ frontend/src/components/Header/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="header">
+    <h1 class="title">
+      {{ title }}
+    </h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.header {
+  height: 64px;
+  width: 100vw;
+  background: #d9d9d9;
+  font-family:Arial, Helvetica, sans-serif;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: center;
+  text-align: start;
+}
+
+.title {
+  margin: 0 10px;
+  color: black;
+  font-size: 1.5rem;
+}
+</style>
