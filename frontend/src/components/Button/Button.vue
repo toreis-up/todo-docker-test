02f0172b991b6e12a671b0f18b4fd6833048a72e
tores-up <toreis@reisan.dev>
2024-01-03 23:31:29 +0900

    f feat(wip): Button

---
+++ frontend/src/components/Button/Button.vue
@@ -0,0 +1,44 @@
+<template>
+    <input :class="'my-btn ' + (disabled ? 'disabled' : '')" type="button" @click="onClick()" :value="label" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    disabled: {
+      type: Boolean,
+      default: () => false
+    }
+  },
+  methods: {
+    onClick() {
+      this.$emit('click')
+    }
+  },
+  emits: ['click']
+})
+</script>
+
+<style lang="scss">
+.my-btn {
+  padding: 10px 20px;
+  border-radius: 10px;
+  background: #45C6EF;
+  color: black;
+
+  &:hover {
+    background-color: #45EFB2;
+  }
+}
+
+.disabled{
+    background-color: #A7EAFF;
+    color: #999;
+  }
+</style>
